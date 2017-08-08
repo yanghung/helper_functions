8fb4eddc9cc5259dd2d35391c0b45fcfe9a0109b
Yang Hung <yanghung@justin.tv>
2017-08-08 16:54:37 -0700

    add tooltip to composite chart

--- mode_helper.js
+++ mode_helper.js
@@ -217,8 +217,7 @@ function createCompositeChart(the_chart, options, data_series_array) {
   // (options.resize) ? the_chart.useViewBoxResizing(options.resize): the_chart.useViewBoxResizing(false);
   // (options.num_clip_padding) ? the_chart.clipPadding(options.num_clip_padding) : null; //if you set clipPadding then include it in options, otherwise do nothing
   // (options.x_ordering) ? the_chart.ordering(options.x_ordering) : null;
-  (options.key_accessor) ? the_chart_obj.keyAccessor(options.key_accessor): null;
-  (options.value_accessor) ? the_chart_obj.valueAccessor(options.value_accessor): null;
+  (options.tooltip) ? the_chart_obj.title(options.tooltip): null; //if you set title then include it in options, otherwise do nothing
 };
 
 
